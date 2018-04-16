fe5e109bd771fe71e775b70de8d6b289345ae611
Dg <dicamarques14@users.noreply.github.com>
2018-04-16 21:22:52 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import time
 import requests
 import json
 
+token_key = '123213213213123'
+channel_ID = "123"
+
 client = discord.Client()
 
 def getCharStatus(accountName):
@@ -15,38 +18,34 @@ def getCharStatus(accountName):
 async def my_background_task():
     await client.wait_until_ready()
     counter = 0
-    channel = discord.Object(id="channelid") #channelID to show messages
+    channel = discord.Object(id=channel_ID) #channelID to show messages
     while not client.is_closed:
+        #print('Time To check')
         for account in config['accounts']:
             jsonAccount = getCharStatus(account['accountName'])
-            #print(jsonAccount)
             for jsonCharac in jsonAccount:
+                #print(jsonCharac['league'])
+                if jsonCharac['league'] == 'Hardcore Bestiary':
+                    if jsonCharac['name'] not in account['aliveChars']:
+
+                        msgNew = account['accountName'] + ' gives another try, ha ha lets see how long it lasts MUUAHAHAH!! ' + jsonCharac['name']
+                        print(msgNew)
+                        await client.send_message(channel,msgNew)
+                        account['aliveChars'].append(jsonCharac['name'])
+                        with open('config.json', 'w') as f:
+                            json.dump(config, f)
+
                 for configCharac in account['aliveChars']: 
-                   
                     if jsonCharac['name'] == configCharac:
                         if jsonCharac['league'] != 'Hardcore Bestiary':
-                            msgDead = 'HA HA, U DED, LUL NOOB: ' + jsonCharac['name'] + ' died at level ' + jsonCharac['level']
+
+                            msgDead = 'HA HA, U DED, LUL NOOB: ' + jsonCharac['name'] + ' died at level ' + str(jsonCharac['level'])
                             await client.send_message(channel,msgDead)
                             print(msgDead)
                             account['aliveChars'].remove(jsonCharac['name'])
                             #write it back to the file
                             with open('config.json', 'w') as f:
                                 json.dump(config, f)
-                    else:
-                        if jsonCharac['league'] == 'Hardcore Bestiary':
-                            if jsonCharac['name'] in account['aliveChars']:
-                                continue
-                            else:
-                                msgNew = account['accountName'] + ' gives another try, ha ha lets see how long it lasts MUUAHAHAH!! ' + jsonCharac['name']
-                                print(msgNew)
-                                await client.send_message(channel,msgNew)
-                                account['aliveChars'].append(jsonCharac['name'])
-                                with open('config.json', 'w') as f:
-                                    json.dump(config, f)
-                            
-                
-            
-        #await client.send_message(channel, counter)
         await asyncio.sleep(60) # task runs every 60 seconds
 
 @client.event
@@ -60,9 +59,11 @@ async def on_message(message):
         accountName = message.content.split()
         if len(accountName) == 2:
             await client.send_message(message.author, 'Account ' + accountName[1] +' added. Happy deaths :)')
-            account = {}
-            account['accountName'] = accountName[1]
-            account['aliveChars'] = {}
+            print('New Account: '+accountName[1])
+            account = {
+                "accountName" : accountName[1],
+                "aliveChars" : []
+            }
             config['accounts'].append(account)
             with open('config.json', 'w') as f:
                 json.dump(config, f)
@@ -77,4 +78,4 @@ with open('config.json', 'r') as f:
 
 
 client.loop.create_task(my_background_task())
-client.run('token')
+client.run(token_key)
