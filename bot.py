729148cceddd1bd858b0978f3cfa5a5cce12cd6f
Dg <dicamarques14@users.noreply.github.com>
2018-04-20 09:36:01 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,11 +3,14 @@ import asyncio
 import time
 import requests
 import json
+from random import randint
 
-token_key = '123213213213123'
-channel_ID = "123"
+token_key = '1321654156'
+channel_ID = "1231531"
 
 client = discord.Client()
+config = {}
+config['hello'] = 1
 
 def getCharStatus(accountName):
     r = requests.post('https://www.pathofexile.com/character-window/get-characters', data = {'accountName':accountName})
@@ -15,45 +18,71 @@ def getCharStatus(accountName):
     jsonArray = json.loads(jsonArray)
     return jsonArray
 
+def getDeathLine():
+    switcher = [
+        "For those who ascend too quickly the fall is inevitable .",
+        "Why are you so in love with death ?",
+        "I fought for God. What did you fight for exile ?",
+        "THE TOUCH OF GOD ! ",
+        "Justice is served.",
+        "Apparently, this is the ending that you deserve."
+    ]
+    return switcher[randint(0, len(switcher))]
+
 async def my_background_task():
+
+    global config
+    load_config()
+    await asyncio.sleep(5)
     await client.wait_until_ready()
     counter = 0
     channel = discord.Object(id=channel_ID) #channelID to show messages
     while not client.is_closed:
         #print('Time To check')
-        for account in config['accounts']:
-            jsonAccount = getCharStatus(account['accountName'])
-            for jsonCharac in jsonAccount:
-                #print(jsonCharac['league'])
-                if jsonCharac['league'] == 'Hardcore Bestiary':
-                    if jsonCharac['name'] not in account['aliveChars']:
+        if 'accounts' in config:
+            for account in config['accounts']:
+                jsonAccount = getCharStatus(account['accountName'])
+                for jsonCharac in jsonAccount:
+                    #print(jsonCharac['league'])
+                    if jsonCharac['league'] == 'Hardcore Bestiary':
+                        if jsonCharac['name'] not in account['aliveChars']:
 
-                        msgNew = account['accountName'] + ' gives another try, ha ha lets see how long it lasts MUUAHAHAH!! ' + jsonCharac['name']
-                        print(msgNew)
-                        await client.send_message(channel,msgNew)
-                        account['aliveChars'].append(jsonCharac['name'])
-                        with open('config.json', 'w') as f:
-                            json.dump(config, f)
-
-                for configCharac in account['aliveChars']: 
-                    if jsonCharac['name'] == configCharac:
-                        if jsonCharac['league'] != 'Hardcore Bestiary':
-
-                            msgDead = 'HA HA, U DED, LUL NOOB: ' + jsonCharac['name'] + ' died at level ' + str(jsonCharac['level'])
-                            await client.send_message(channel,msgDead)
-                            print(msgDead)
-                            account['aliveChars'].remove(jsonCharac['name'])
-                            #write it back to the file
+                            msgNew = 'Welcome to Wraeclast '+ jsonCharac['name'] + '. Good luck on your journey'
+                            print(msgNew)
+                            await client.send_message(channel,msgNew)
+                            account['aliveChars'].append(jsonCharac['name'])
                             with open('config.json', 'w') as f:
                                 json.dump(config, f)
-        await asyncio.sleep(60) # task runs every 60 seconds
+
+                    for configCharac in account['aliveChars']: 
+                        if jsonCharac['name'] == configCharac:
+                            if jsonCharac['league'] != 'Hardcore Bestiary':
+
+                                msgDead = getDeathLine() + ' ' + jsonCharac['name'] + ' died at level ' + str(jsonCharac['level'])
+                                await client.send_message(channel,msgDead)
+                                print(msgDead)
+                                account['aliveChars'].remove(jsonCharac['name'])
+                                #write it back to the file
+                                with open('config.json', 'w') as f:
+                                    json.dump(config, f)
+            await asyncio.sleep(60) # task runs every 60 seconds
+        else:
+            print("no account")
+
+def load_config():
+    global config
+    with open('config.json', 'r') as f:
+         config = json.load(f)
 
 @client.event
 async def on_ready():
+    load_config()
     print('Bot ON')
 
 @client.event
 async def on_message(message):
+    
+    global config
     if message.content.startswith('?add'):
         
         accountName = message.content.split()
@@ -71,11 +100,6 @@ async def on_message(message):
             await client.send_message(message.author, 'Usage: ?add <accountname>')
 
         
-    
-with open('config.json', 'r') as f:
-    config = json.load(f)
-
-
 
 client.loop.create_task(my_background_task())
 client.run(token_key)
